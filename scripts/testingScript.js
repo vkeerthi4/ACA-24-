ed0997ef83f7106707686ccef57321d075d2ffaf
vkeerthi4 <90127122+vkeerthi4@users.noreply.github.com>
2021-09-18 17:53:47 +0530

    Add files via upload

---
+++ scripts/testingScript.js
@@ -0,0 +1,12 @@
+/* Configuration details - CDN from firebase */
+
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyBaOY-K9lYkw55i6HdmDbTS3N5pyARUbzE",
+    authDomain: "sports-website-5a586.firebaseapp.com",
+    projectId: "sports-website-5a586",
+    storageBucket: "sports-website-5a586.appspot.com",
+    messagingSenderId: "380371018973",
+    appId: "1:380371018973:web:f5e71907287ac9c15a6734"
+  };
+  firebase.initializeApp(firebaseConfig);
